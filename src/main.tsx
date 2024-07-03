6c5df59209ee580aaac30ae21f2873197f29bdd1
GreeningSiren <greeningsiren@gmail.com>
2024-07-03 20:21:15 +0300

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { DiscordSDK } from '@discord/embedded-app-sdk'
+
+const discordSDK = new DiscordSDK(import.meta.env.DISCORD_CLIENT_ID)
+// @ts-expect-error The discord sdk reads the setupDiscordSdk (i think)
+async function setupDiscordSdk() {
+  await discordSDK.ready();
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
